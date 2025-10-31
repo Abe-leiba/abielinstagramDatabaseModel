162fa6b5c46ec5704187cbad504584085b905153
Abe-leiba <abel_con@yahoo.com>
2025-10-31 18:41:56 +0000

    instagram models

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
 
@@ -10,6 +11,8 @@ class User(db.Model):
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    #relationship
+    posts: Mapped[List["Post"]] = relationship(back_populates = "user")
 
     def serialize(self):
         return {
@@ -17,3 +20,61 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Post(db.Model):
+     id: Mapped[int] = mapped_column(primary_key=True)
+
+     #relationship
+     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+     def serialize(self):
+        return {
+            "id": self.id,
+        
+            
+        }
+
+
+
+class Follower(db.Model):
+     id:Mapped[int] = mapped_column(primary_key=True)
+     
+     #relationship
+     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+     follower_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+     def serialize(self):
+        return {
+            "id": self.id,
+        
+            
+        }
+     
+     
+class Comment(db.Model):
+     id: Mapped[int] = mapped_column(primary_key=True)
+     comment_text: Mapped[str] = mapped_column(String(120))
+     #relationship
+     author_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+     post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+     def serialize(self):
+        return {
+            "id": self.id,
+        
+            
+        }
+     
+     
+class Media(db.Model):
+     id: Mapped[int] = mapped_column(primary_key=True)
+
+     #relationship
+     post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+     url: Mapped[str] = mapped_column(String(250))
+     
+     def serialize(self):
+        return {
+            "id": self.id,
+        
+            
+        }
+     
